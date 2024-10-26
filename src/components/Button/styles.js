26b0662f5525c0b2f90ce01473001d9bc03db7d0
gununes031 <gununes31@yahoo.com.br>
2024-10-26 11:50:46 -0300

    entrega desafio dio

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const ButtonContainer = styled.button`
     padding: 20px;
     border: 1px solid #CDCDCD;
+    border-radius: 5px;
     background-color: #00AAF0;
     color: #FFFFFF;
     font-size: 24px;
